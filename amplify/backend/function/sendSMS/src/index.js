0a19663516497b8dbf49e908738480de68809b7c
Mike Moore <mike.moore@so.engineering>
2021-09-18 23:53:17 -0500

    Add lambda to send sms.

--- amplify/backend/function/sendSMS/src/index.js
+++ amplify/backend/function/sendSMS/src/index.js
@@ -1,25 +1,33 @@
 const aws = require('aws-sdk');
 
-const client = require('twilio')(
-  'ACcfcfc89713b9c606086be57628a2e016',
-  '79c7127390f01a8d15b3e6524bb170a7'
-);
-
 exports.handler = async (event) => {
+    // Load secret twilio API auth params.
     const { Parameters } = await (new aws.SSM())
       .getParameters({
-        Names: ["TWILIO_ACCOUNT_SID","TWILIO_AUTH_TOKEN","TWILIO_PHONE_NUMBER"].map(secretName => process.env[secretName]),
+        Names: ["TWILIO_ACCOUNT_SID","TWILIO_AUTH_TOKEN","TWILIO_NOTIFY_SERVICE_SID"].map(secretName => process.env[secretName]),
         WithDecryption: true,
     }).promise();
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify(Parameters),
-    };
-    return response;
+    TWILIO_ACCOUNT_SID = Parameters[0].Value
+    TWILIO_AUTH_TOKEN = Parameters[1].Value
+    TWILIO_NOTIFY_SERVICE_SID = Parameters[2].Value
+    // Instantiate twilio client API
+    const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN); 
+    // Use twilio notification service to send many SMS messages in one API call
+    const service = twilio.notify.services(TWILIO_NOTIFY_SERVICE_SID);
+    // Create an sms binding for each recipient
+    const bindings = event.recipients.map(recipient => {
+      return JSON.stringify({ binding_type: 'sms', address: recipient });
+    });
+    // Send messages out using our twilio notification service
+    return service.notifications
+      .create({
+            toBinding: bindings,
+            body: event.message,
+      })
+      .then(notification => {
+            console.log(notification);
+      })
+      .catch(err => {
+            console.log(err);
+      });
 };
